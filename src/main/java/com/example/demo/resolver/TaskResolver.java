2649179d6f4c11cafa09d8fe6aa4a9d7748d3bcc
Lybio Moraes Junior <j.lybio@aluno.ifsp.edu.br>
2025-06-07 15:51:30 -0300

    refactor: use autowired

--- src/main/java/com/example/demo/resolver/TaskResolver.java
+++ src/main/java/com/example/demo/resolver/TaskResolver.java
@@ -2,6 +2,7 @@ package com.example.demo.resolver;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 
@@ -11,11 +12,8 @@ import com.example.demo.service.interfaces.TaskService;
 @org.springframework.stereotype.Controller
 public class TaskResolver {
 
-    private final TaskService taskService;
-
-    public TaskResolver(TaskService taskService) {
-        this.taskService = taskService;
-    }
+    @Autowired
+    private TaskService taskService;
 
     @QueryMapping
     public List<TaskResponseDto> getAllTasks() {
